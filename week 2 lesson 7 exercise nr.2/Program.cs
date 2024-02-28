b46cb44951b00fc64dea37d0daacde6fe1b07826
konrad101296 <konradslimak@o2.pl>
2024-02-28 19:07:27 +0100

    Add project files.

---
+++ week 2 lesson 7 exercise nr.2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("podaj liczbe:");
+int number = int.Parse(Console.ReadLine());
+
+int wynik = number % 2;
+
+if (wynik == 1)
+{
+    Console.WriteLine($"{number} jest liczba nieparzystą");
+}
+else
+{
+    Console.WriteLine($"{number} jest liczba parzystą");
+}
